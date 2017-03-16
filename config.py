082eead8b04bc6e1d3a98266177c7650f53e478e
polandy <pollari@gmx.ch>
2017-03-16 21:54:19 +0100

    added jenkins config params

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 from model import Section
 
+jenkins_url = "https://jenkins.example.com"
+jenkins_username = "jenkins_user"
+jenkins_password = "top_secret"
+
 sections = [
     Section("development", 0, 9, ["dev", "dev-test"]),
     Section("sonar", 10, 19, ["sonar"]),
