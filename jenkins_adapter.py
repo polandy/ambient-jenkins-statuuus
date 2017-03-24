ff892e8989db78c23c36b51bd87556970759ea13
polandy <pollari@gmx.ch>
2017-03-24 19:30:31 +0100

    use job name pattern to get build infos

--- jenkins_adapter.py
+++ jenkins_adapter.py
@@ -9,18 +9,25 @@ server = jenkins.Jenkins(config.jenkins_url, username=config.jenkins_username, p
 
 
 def get_last_build_number(project_name):
-    return server.get_job_info(project_name)['lastBuild']['number']
+    project_last_build_number = dict()
+    for project in server.get_job_info_regex(project_name):
+        if project['lastBuild'] is not None:
+            project_last_build_number[project['displayName']] = project['lastBuild']['number']
+    return project_last_build_number
 
 
 def get_build_status(project_name):
-    build_number = get_last_build_number(project_name)
-    return server.get_build_info(project_name, build_number)['result']
+    build_numbers = get_last_build_number(project_name)
+    build_results = []
+    for project_name, build_number in build_numbers.iteritems():
+        build_results.append(server.get_build_info(project_name, build_number)['result'])
+    return build_results
 
 
 def status_to_numbers(argument):
     switcher = {
         "FAILED" : -1,
-        "UNSTABLE" : 0,
+        "UNSTABLE": 0,
         "SUCCESS": 1,
     }
     return switcher.get(argument, -1)
@@ -32,11 +39,14 @@ def get_section_state_dict():
     for section in config.sections:
         for project_name in section.project_names:
             try:
-                build_status = get_build_status(project_name)
-                if section in section_state:
-                    section_state[section] = build_status if status_to_numbers(build_status) < status_to_numbers(section_state[section]) else section_state[section]
-                else:
-                    section_state[section] = build_status
+                build_states = get_build_status(project_name)
+                for state in build_states:
+                    if section in section_state:
+                        section_state[section] = state if status_to_numbers(state) < status_to_numbers(section_state[section]) else section_state[section]
+                    else:
+                        section_state[section] = state
             except jenkins.NotFoundException:
                 print 'WARNING: configured project "%s" for section "%s" not found' % (project_name, section.name)
     return section_state
+
+get_section_state_dict()
