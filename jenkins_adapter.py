60068d01cb4f32b2b65f247e13f4b0def305e1f7
caebr <claudia@asti.ch>
2017-06-22 16:22:58 +0200

    prepared jenkins adapter for multibranch pipeline using new class job

--- jenkins_adapter.py
+++ jenkins_adapter.py
@@ -6,21 +6,24 @@ from datetime import datetime
 server = jenkins.Jenkins(config.jenkins_url, username=config.jenkins_username, password=config.jenkins_password)
 
 
-def get_last_build_number(project_name):
+def get_last_build_number(job):
     project_last_build_number = dict()
-    for project in server.get_job_info_regex(project_name):
+    if job.pipeline:
+        print "todo pipeline"
+    else:
+        project = server.get_job_info(job.name)
         if project['lastBuild'] is not None:
             project_last_build_number[project['displayName']] = project['lastBuild']['number']
     return project_last_build_number
 
 
-def get_build_status(project_name):
-    build_numbers = get_last_build_number(project_name)
+def get_build_status(job):
+    build_numbers = get_last_build_number(job)
     build_results = []
-    for project_name, build_number in build_numbers.iteritems():
-        build_info = server.get_build_info(project_name, build_number)
+    for job, build_number in build_numbers.iteritems():
+        build_info = server.get_build_info(job, build_number)
         if build_info['building'] and build_number > 0:
-            build_info = server.get_build_info(project_name, build_number-1)
+            build_info = server.get_build_info(job, build_number - 1)
             build_results.append(build_info['result'] + '_building')
         build_results.append(build_info['result'])
     return build_results
@@ -28,8 +31,8 @@ def get_build_status(project_name):
 
 def status_to_numbers(argument):
     switcher = {
-        "FAILED_building" : -4,
-        "FAILED" : -3,
+        "FAILED_building": -4,
+        "FAILED": -3,
         "UNSTABLE_building": -2,
         "UNSTABLE": -1,
         "SUCCESS_building": 0,
@@ -42,16 +45,16 @@ def get_section_state_dict():
     global section
     section_state = dict()
     for section in config.sections:
-        for project_name in section.project_names:
+        for job in section.jobs:
             try:
-                build_states = get_build_status(project_name)
+                build_states = get_build_status(job)
                 for state in build_states:
                     if section in section_state:
                         section_state[section] = state if status_to_numbers(state) < status_to_numbers(section_state[section]) else section_state[section]
                     else:
                         section_state[section] = state
             except jenkins.NotFoundException:
-                print 'WARNING: configured project "%s" for section "%s" not found' % (project_name, section.name)
+                print 'WARNING: configured project "%s" for section "%s" not found' % (job, section.name)
             except jenkins.JenkinsException:
                 print '\t ----------------WARNING: error occured (JenkinsException):---------------------- '
                 print 'INFO: datetime: %s' % (str(datetime.date()))
