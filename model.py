9739120fa519a1047080a84129c3796951413ef6
caebr <claudia@asti.ch>
2017-06-22 15:59:25 +0200

    added new job class which is used in section

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 class Section(object):
 
-    def __init__(self, name, range_start, range_end, project_names):
+    def __init__(self, name, range_start, range_end, jobs):
         self.name = name
         self.range_start = range_start
         self.range_end = range_end
-        self.project_names = project_names
+        self.jobs = jobs
 
 
 class Led(object):
@@ -12,3 +12,10 @@ class Led(object):
     def __init__(self, primary_color, secondary_color):
         self.primary_color = primary_color
         self.secondary_color = secondary_color
+
+
+class Job(object):
+
+    def __init__(self, name, pipeline=False):
+        self.name = name
+        self.pipeline = pipeline
