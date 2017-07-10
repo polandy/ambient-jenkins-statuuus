0fdca3da9d3df9e728c1fe840571caf5879ef1e0
caebr <claudia@asti.ch>
2017-07-10 11:43:56 +0200

    removed timestamp from exception log

--- jenkins_adapter.py
+++ jenkins_adapter.py
@@ -1,7 +1,6 @@
 import config
 import jenkins
 import traceback
-from datetime import datetime
 
 server = jenkins.Jenkins(config.jenkins_url, username=config.jenkins_username, password=config.jenkins_password)
 
@@ -62,7 +61,6 @@ def get_section_state_dict():
                 print 'WARNING: configured job "%s" for section "%s" not found' % (job.name, section.name)
             except jenkins.JenkinsException:
                 print '\t ----------------WARNING: error occured (JenkinsException):---------------------- '
-                print 'INFO: datetime: %s' % (str(datetime.date()))
                 print traceback.print_exc()
                 print '\t --------------------------------------------------------------------------------- '
     return section_state
